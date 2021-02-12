21882f0c075ed072ccd430b4f7910d7bc82c2d59
MaherHassanain <maher_hassanain@hotmail.com>
2021-02-12 20:01:22 +0300

    added exchange rate to recalculate unit price

--- Main.java
+++ Main.java
@@ -90,8 +90,73 @@ public class Main {
 		System.out.println(totalPriceForAllItems);
 	  }
 	
-	static void nonRiyalsFunction() {
+	static void nonRiyalsFunction(double x) {
 		// put code here
+		
+	    System.out.println("Rate is: " + x);
+		
+		try {
+			File file = new File("./src/snc/com/sample.xlsx");
+			FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file  
+			//creating Workbook instance that refers to .xlsx file  
+			XSSFWorkbook wb = new XSSFWorkbook(fis);   
+			XSSFSheet sheet = wb.getSheetAt(0);     //creating a Sheet object to retrieve object  
+			Iterator<Row> itr = sheet.iterator();    //iterating over excel file  
+			while(itr.hasNext()) {
+				Row row = itr.next();  
+				Iterator<Cell> cellIterator = row.cellIterator();   //iterating over each column  
+				while(cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();  
+					switch (cell.getCellType()) {						
+					case STRING: {
+						// do something
+//						System.out.print(cell.getStringCellValue() + "\t\t\t");
+						break;
+					}
+					case NUMERIC: {
+						// do something else
+//						System.out.print(cell.getNumericCellValue() + "\t\t\t");
+						EXCELCELLS.add(cell.getNumericCellValue());
+						break;
+					}
+					default: {
+						// do nothing
+					}	
+//					System.out.println(""); 
+				}
+			}
+		}	
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		for(int num = 0; num < EXCELCELLS.size(); num++) {
+			num++;
+			Quantity.add(EXCELCELLS.get(num));
+			num++;
+			UnitPriceinRiyals.add(EXCELCELLS.get(num));
+			num++;
+		}
+		
+		for(int i = 0; i < Quantity.size(); i++) {
+			double temp = 0;
+			temp = Quantity.get(i)*UnitPriceinRiyals.get(i)*x;
+			TotalPriceEach.add(temp);
+		}
+		
+		for (int j = 0; j < TotalPriceEach.size(); j++) {
+			totalPriceForAllItems = totalPriceForAllItems + TotalPriceEach.get(j);
+		}
+		
+//		System.out.println(Quantity.get(0));
+//		System.out.println(UnitPriceinRiyals.get(0));
+//		System.out.println(Quantity.get(1));
+//		System.out.println(UnitPriceinRiyals.get(1));
+//		System.out.println(TotalPriceEach.get(0));
+//		System.out.println(TotalPriceEach.get(1));
+//		System.out.println(TotalPriceEach.get(16));
+		System.out.println(totalPriceForAllItems);
 	}
 	
 	public static void main(String[] args) {
@@ -109,7 +174,7 @@ public class Main {
 		    	
 		    } else {
 		    	// means it is not in Saudi riyals
-		    	nonRiyalsFunction();
+		    	nonRiyalsFunction(cRate);
 		    }		
 	}
 }
